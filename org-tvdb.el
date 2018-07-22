c1c41c01cabcdfda42f4a0a1cf09ffb599e231cb
reims <rbackhaus@gmail.com>
2018-07-22 21:28:38 +0200

    add a lot of missing functionality

--- org-tvdb.el
+++ org-tvdb.el
@@ -27,24 +27,45 @@
 (require 's)
 (require 'deferred)
 (require 'request-deferred)
+(require 'cl-lib)
 
-(defcustom org-tvdb-api-key nil
+(defgroup org-tvdb nil
+  "Customization group for the org-tvdb package."
+  :group 'outlines)
+
+(defcustom org-tvdb-api-key ""
   "API key for tvdb.org."
-  :type 'string)
+  :type 'string
+  :group 'org-tvdb)
 
 (defcustom org-tvdb-released-status "RELEASED"
   "Todo status for released but unwatched episodes."
-  :type 'string)
+  :type 'string
+  :group 'org-tvdb)
 
 (defcustom org-tvdb-unreleased-status "UNRELEASED"
   "Todo status for unreleased episodes."
-  :type 'string)
+  :type 'string
+  :group 'org-tvdb)
 
 (defcustom org-tvdb-watched-status "WATCHED"
   "Todo status for watched episodes."
-  :type 'string)
+  :type 'string
+  :group 'org-tvdb)
+
+(defcustom org-tvdb-agenda-command
+  '("e" "Unwatched episodes" todo "RELEASED"
+    ((org-agenda-files '("~/dev/org-tvdb/test.org"))))
+  "Custom agenda commands to show released but not watched episdes."
+  :type 'sexp
+  :group 'org-tvdb)
 
 (defvar org-tvdb--token nil)
+
+;;;###autoload
+(defun org-tvdb-released-tree ()
+  (interactive)
+  (org-occur (s-concat "^\\*+ " org-tvdb-released-status)))
 
 ;;;###autoload
 (defun org-tvdb-insert-todo-list ()
@@ -92,14 +113,14 @@
 		      :headers `(("Authorization" . ,(s-concat "Bearer " org-tvdb--token)))
 		      :params params)))
 
-(defun org-tvdb--query (query-fn finalize-fn end-point &optional params)
+(defun org-tvdb--query (finalize-fn end-point &optional params)
   "Query END-POINT with PARAMS and login if necessary.
 
 The query is done with QUERY-FN.  QUERY-FN must return a deferred
 object that returns a response.  The result of calling
 FINALIZE-FN on the response is returned."
   (deferred:$
-    (funcall query-fn end-point params)
+    (org-tvdb--query-request end-point params)
     (deferred:nextc it
       (lambda (response)
 	(case (request-response-status-code response)
@@ -108,7 +129,7 @@ FINALIZE-FN on the response is returned."
 		 (org-tvdb--login)
 		 (deferred:nextc it
 		   (lambda (_)
-		     (funcall query-fn end-point params)))
+		     (org-tvdb--query-request end-point params)))
 		 (deferred:nextc it
 		   (lambda (response)
 		     (if (= 200 (request-response-status-code response))
@@ -125,31 +146,39 @@ FINALIZE-FN on the response is returned."
 
 (defun org-tvdb--move-to-series ()
   "Move to the first parent that has a TVDBID property."
-  (let ((id (org-entry-get (point) "TVDBID")))
+  (let ((id (org-entry-get (point) "TVDB_SERIES_ID")))
     (while (and (> (org-current-level) 1) (null id))
       (outline-up-heading 1 t)
-      (setq id (org-entry-get (point) "TVDBID")))
+      (setq id (org-entry-get (point) "TVDB_SERIES_ID")))
     (or	id
 	(error "Not in series"))))
 
 (defun org-tvdb--move-to-season ()
   "Move to the first parent that has a TVDB_AIRED_SEASON property."
-  (let ((aired-season (org-entry-get (point) "TVDB_AIRED_SEASON")))
+  (outline-end-of-subtree)
+  (let ((aired-season (or (org-entry-get (point) "TVDB_AIRED_SEASON")
+			  (when (org-entry-get (point) "TVDB_SERIES_ID")
+			    "0"))))
     (while (and (> (org-current-level) 1) (null aired-season))
       (outline-up-heading 1 t)
-      (setq aired-season (org-entry-get (point) "TVDB_AIRED_SEASON")))
+      (setq aired-season (org-entry-get (point) "TVDB_AIRED_SEASON"))
+      (when (org-entry-get (point) "TVDB_SERIES_ID")
+	(setq aired-season "0")))
     (or aired-season
-	(error "Not in season"))))
+	(error "Not in series"))))
 
-(parse-time-string "2017-01-01")
-
-(defun org-tvdb--has-been-released? (episode)
+(defun org-tvdb--unreleasedp (episode)
   "Return non-nil if first aired date of EPISODE is in the past."
-  (destructuring-bind (_ _ _ current-day current-month current-year _ _ _) (decode-time (current-time))
-    (destructuring-bind (_ _ _ release-day release-month release-year _ _ _) (parse-time-string (alist-get 'firstAired episode))
-      (and (>= current-year release-year)
-	   (>= current-month release-month)
-	   (>= current-day release-day)))))
+  (let ((firstAired (alist-get 'firstAired episode)))
+    (or (or (null firstAired) (zerop (length firstAired)))
+      (destructuring-bind (_ _ _ current-day current-month current-year _ _ _) (decode-time (current-time))
+	(destructuring-bind (_ _ _ release-day release-month release-year _ _ _) (parse-time-string firstAired)
+	  (cond
+	   ((> release-year current-year) t)
+	   ((< release-year current-year) nil)
+	   ((> release-month current-month) t)
+	   ((< release-month current-month) nil)
+	   ((> release-day current-day) t)))))))
 
 (defun org-tvdb--insert-episode (episode)
   "Insert episode defined by the alist EPISODE."
@@ -157,13 +186,14 @@ FINALIZE-FN on the response is returned."
       (org-insert-subheading nil)
     (org-insert-heading))
   (insert (alist-get 'episodeName episode))
-  (if (org-tvdb--has-been-released? episode)
-      (org-todo org-tvdb-released-status)
-    (org-todo org-tvdb-unreleased-status))
+  (if (org-tvdb--unreleasedp episode)
+      (org-todo org-tvdb-unreleased-status)
+    (org-todo org-tvdb-released-status))
   (org-return t)
-  (insert (alist-get 'overview episode))
+  (if-let ((overview (alist-get 'overview episode)))
+    (insert overview))
   (fill-paragraph)
-  (org-entry-put (point) "TVDB_EPISODEID" (number-to-string (alist-get 'id episode)))
+  (org-entry-put (point) "TVDB_EPISODE_ID" (number-to-string (alist-get 'id episode)))
   (org-entry-put (point) "TVDB_LAST_UPDATE" (number-to-string (alist-get 'lastUpdated episode)))
   (org-entry-put (point) "TVDB_FIRST_AIRED" (alist-get 'firstAired episode))
   (org-entry-put (point) "TVDB_NUMBER_IN_SEASON" (number-to-string (alist-get 'airedEpisodeNumber episode)))
@@ -183,6 +213,7 @@ FINALIZE-FN on the response is returned."
        ((< diff 0) (dotimes (_ (* -1 diff))
 		     (org-demote-subtree)))))
     (org-entry-put (point) "TVDB_AIRED_SEASON" (number-to-string season))
+    (outline-end-of-subtree)
     (org-return t))
   (let ((episodes (alist-get 'data data)))
     (dotimes (idx (length episodes))
@@ -193,35 +224,77 @@ FINALIZE-FN on the response is returned."
 
 (defun org-tvdb--fetch-season (series-id season)
   "Fetch the episodes for season number SEASON from series SERIES-ID."
-  (let ((end-point (s-concat "series/" series-id "/episodes/query")))
-    (org-tvdb--query #'org-tvdb--query-request #'request-response-data end-point `(("airedSeason" . ,season)))))
+  (let ((end-point (s-concat "series/"
+			     (if (numberp series-id)
+				 (number-to-string series-id)
+			       series-id)
+			     "/episodes/query")))
+    (org-tvdb--query #'request-response-data
+		     end-point
+		     `(("airedSeason" . ,season)))))
 
 (defun org-tvdb--fetch-series (series-id)
   "Fetch series with id SERIES-ID."
-  (let ((end-point (s-concat "series/" series-id)))
-    (org-tvdb--query #'org-tvdb--query-request #'request-response-data end-point)))
+  (let ((end-point (s-concat "series/"
+			     (if (numberp series-id)
+				 (number-to-string series-id)
+			       series-id))))
+    (org-tvdb--query #'request-response-data
+		     end-point)))
 
 (defun org-tvdb--fetch-series-head (series-id)
   "Fetch series HEAD with id SERIES-ID."
-  (let ((end-point (s-concat "series/" series-id)))
-    (message "end-point %s" end-point)
-    (org-tvdb--query #'org-tvdb--query-request #'identity end-point)))
+  (let ((end-point (s-concat "series/"
+			     (if (numberp series-id)
+				 (number-to-string series-id)
+			       series-id))))
+    (org-tvdb--query #'identity
+		     end-point)))
 
 (defun org-tvdb--fetch-series-summary (series-id)
   "Fetch summary for series with SERIES-ID."
-  (org-tvdb--query #'org-tvdb--query-request #'request-response-data (s-concat "series/" series-id "/episodes/summary")))
+  (let ((end-point (s-concat "series/"
+			     (if (numberp series-id)
+				 (number-to-string series-id)
+			       series-id)
+			     "/episodes/summary")))
+    (org-tvdb--query #'request-response-data
+		     end-point)))
 
 (defun org-tvdb--search-series (name)
   "Search for series with name NAME."
   (let ((end-point "search/series"))
-    (org-tvdb--query #'org-tvdb--query-request #'request-response-data end-point `(("name" . ,name)))))
+    (org-tvdb--query #'request-response-data
+		     end-point
+		     `(("name" . ,name)))))
 
 (defun org-tvdb--insert-series-header (series)
   "Insert header for series SERIES."
   (org-insert-heading)
   (insert (alist-get 'seriesName series))
-  (org-entry-put (point) "TVDBID" (number-to-string (alist-get 'id series)))
+  (org-entry-put (point) "TVDB_SERIES_ID" (number-to-string (alist-get 'id series)))
   (org-entry-put (point) "LAST_UPDATE" (number-to-string (org-tvdb--current-millis))))
+
+;;;###autoload
+(defun org-tvdb-add-season (season)
+  "Add season number SEASON of current series."
+  (interactive "nSeason: ")
+  (let ((id (org-tvdb--move-to-series)))
+    (deferred:$
+      (org-tvdb--fetch-season id season)
+      (deferred:nextc it
+	(lambda (data)
+	  (org-tvdb--insert-season season data))))))
+
+(defun org-tvdb--add-seasons (seasons)
+  "Fetch all seasons in SEASONS from series at point."
+  (unless (zerop (length seasons))
+    (let ((idx 0))
+      (deferred:nextc (org-tvdb-add-season (elt seasons idx))
+	(deferred:lambda (_)
+	  (when (< (incf idx) (length seasons))
+	    (deferred:nextc (org-tvdb-add-season (elt seasons idx))
+	      self)))))))
 
 ;;;###autoload
 (defun org-tvdb-add-series (name)
@@ -234,19 +307,43 @@ FINALIZE-FN on the response is returned."
 	(let ((results (alist-get 'data data)))
 	  (if (zerop (length results))
 	      (error "No such series")
-	    (org-tvdb--insert-series-header (elt results 0))))))))
+	    (let* ((series (elt results 0))
+		   (id (alist-get 'id series)))
+	      (org-tvdb--insert-series-header series)
+	      (org-tvdb--fetch-series-summary id))))))
+    (deferred:nextc it
+      (lambda (summary)
+	(let ((seasons (sort (cl-remove-if #'zerop
+					   (map 'array #'string-to-number
+						(alist-get 'airedSeasons (alist-get 'data summary))))
+			     #'<)))
+	  (org-tvdb--add-seasons seasons))))))
 
-(defun org-tvdb-add-season (season)
-  "Add season number SEASON of current series."
-  (interactive "nSeason: ")
-  (let ((id (org-tvdb--move-to-series)))
-    (deferred:$
-      (org-tvdb--fetch-season id season)
-      (deferred:nextc it
-	(lambda (data)
-	  (org-tvdb--insert-season season data))))))
+;;;###autoload
+(defun org-tvdb-update-season ()
+  "Update season at point."
+  (interactive)
+  (let ((season (string-to-number (or (org-tvdb--move-to-season)
+				      (error "Not in season"))))
+	(series (string-to-number (save-excursion (org-entry-get (point) "TVDB_SERIES_ID" t)))))
+    (if (zerop season)
+	(error "Not in season")
+      (deferred:$
+	(org-tvdb--fetch-season series season)
+	(deferred:nextc it
+	  (lambda (season)
+	    (outline-end-of-subtree)
+	    (let* ((last-episode (string-to-number (or (save-excursion (org-entry-get (point) "TVDB_NUMBER_IN_SEASON"))
+						       "0")))
+		   (episodes (sort (cl-remove-if (lambda (ep)
+						   (<= (alist-get 'airedEpisodeNumber ep) last-episode))
+						 (alist-get 'data season))
+				   (lambda (ep1 ep2)
+				     (< (alist-get 'airedEpisodeNumber ep1)
+					(alist-get 'airedEpisodeNumber ep2))))))
+	      (seq-do #'org-tvdb--insert-episode episodes))))))))
 
-(defun org-tvdb--must-update? (head-response)
+(defun org-tvdb--must-updatep (head-response)
   "Check if LAST_UPDATE property is earlier then last-modified header in HEAD-RESPONSE."
   (let ((last-updated-in-db (org-tvdb--time-to-millis (date-to-time (request-response-header head-response "last-modified"))))
 	(last-updated-here (string-to-number (org-entry-get (point) "LAST_UPDATE"))))
@@ -261,35 +358,28 @@ FINALIZE-FN on the response is returned."
       (org-tvdb--fetch-series-head id)
       (deferred:nextc it
 	(lambda (resp)
-	  (if (org-tvdb--must-update? resp)
+	  (if (org-tvdb--must-updatep resp)
 	      (org-tvdb--fetch-series-summary id)
 	    (deferred:cancel it))))
       (deferred:nextc it
 	(lambda (summary)
-	  (message "%S" summary)
 	  (outline-end-of-subtree)
 	  (let* ((aired-season (string-to-number (org-tvdb--move-to-season)))
-		 (seasons (map 'array #'string-to-number (alist-get 'airedSeasons (alist-get 'data summary))))
-		 (new-seasons (remove-if (lambda (x)
-					   (or (= x 0) (<= x aired-season)))
-					 seasons)))
-	    (if (= 0 (length new-seasons))
-		(message "no new seasons")
-	      (progn
-		(message "fetching season 0")
-		(let ((idx 0))
-		  (deferred:nextc (org-tvdb-add-season (elt new-seasons idx))
-		    (deferred:lambda (_)
-		      (when (< (incf idx) (length new-seasons))
-			(message "fetching season %d" (elt new-seasons idx))
-			(deferred:nextc (org-tvdb-add-season (elt new-seasons idx))
-			  self)))))))))))))
+		 (seasons (sort (map 'array #'string-to-number (alist-get 'airedSeasons (alist-get 'data summary)))
+				#'<))
+		 (new-seasons (cl-remove-if (lambda (x)
+					      (or (= x 0) (<= x aired-season)))
+					    seasons)))
+	    (deferred:nextc
+	      (org-tvdb-update-season)
+	      (lambda (_)
+		(org-tvdb--add-seasons new-seasons)))))))))
 
 ;;;###autoload
 (defun org-tvdb-mark-season-watched ()
   "Mark change status of all episodes in current season to `org-tvdb-watched-status'."
   (interactive)
-  (when (org-entry-get (point) "TVDB_EPISODEID")
+  (when (org-entry-get (point) "TVDB_EPISODE_ID")
     (outline-up-heading 1))
   (outline-end-of-subtree)
   (while (outline-get-last-sibling)
@@ -301,7 +391,7 @@ FINALIZE-FN on the response is returned."
   (interactive)
   (org-tvdb--move-to-series)
   (outline-end-of-subtree)
-  (if (org-entry-get (point) "TVDBID")
+  (if (org-entry-get (point) "TVDB_SERIES_ID")
       (error "Empty series"))
   (while (outline-get-last-sibling)
     (org-tvdb-mark-season-watched)))
